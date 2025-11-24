951b8efa9f43459ec4b491e4e3a1cb634fddc558
Antón Figueroa <antfigtel@gmail.com>
2025-11-24 02:10:23 +0100

    css correction

--- quartz/components/AuthorBlock.tsx
+++ quartz/components/AuthorBlock.tsx
@@ -13,32 +13,32 @@ const AuthorBlock: QuartzComponent = ({ fileData, cfg }: QuartzComponentProps) =
   )
 }
 
-AuthorBlock.css = `
-.author-block {
-  display: flex;
-  flex-direction: column;
-  background: var(--light);
-  border: 0px solid var(--lightgray);
-  padding: 0rem 0rem;
-  border-radius: 0px;
-  margin-bottom: 0rem;
-  font-family: var(--headerFont);
-  flex: 0 0 auto;
-}
-.author-block .author-label {
-  font-size: 1rem;
-  color: var(--darkgray);
-  margin: 0 0 0 0;
-  font-family: var(--headerFont);
-  font-weight: 600; /* match semi-bold headings */
-}
-.author-block .author-name {
-  margin: 0;
-  font-weight: 400;
-  font-family: var(--codeFont);
-  color: var(--darkgray);
-  font-size: 0.95rem;
-}
-`
+// AuthorBlock.css = `
+// .author-block {
+//   display: flex;
+//   flex-direction: column;
+//   background: var(--light);
+//   border: 0px solid var(--lightgray);
+//   padding: 0rem 0rem;
+//   border-radius: 0px;
+//   margin-bottom: 0rem;
+//   font-family: var(--headerFont);
+//   flex: 0 0 auto;
+// }
+// .author-block .author-label {
+//   font-size: 1rem;
+//   color: var(--darkgray);
+//   margin: 0 0 0 0;
+//   font-family: var(--headerFont);
+//   font-weight: 600; /* match semi-bold headings */
+// }
+// .author-block .author-name {
+//   margin: 0;
+//   font-weight: 400;
+//   font-family: var(--codeFont);
+//   color: var(--darkgray);
+//   font-size: 0.95rem;
+// }
+// `
 
 export default (() => AuthorBlock) satisfies QuartzComponentConstructor
